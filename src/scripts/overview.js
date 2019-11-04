93101315b30dfb67a0f05a0a0d7548fbd6d5a839
jannick vandaele <jannickvandaele@hotmail.com>
2019-11-04 20:25:00 +0100

    genres

--- src/scripts/overview.js
+++ src/scripts/overview.js
@@ -6,17 +6,54 @@ export function initOverview() {
     getData();
 }
 
+function getMovies(){
+    return Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + KEY)
+}
+
+function getGenres(){
+    return Axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=" + KEY)
+}
+
 let movies;
+let genres;
 function getData(){
-    Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + KEY)
-    .then(response => {
-        movies = response.data.results;  
-        renderPage();      
-    });
+    Axios.all([getMovies(), getGenres()])
+    .then(Axios.spread(function (moviesResponse, genresResponse) {
+        movies = moviesResponse.data.results;
+        genres = genresResponse.data.genres;
+
+        renderPage();
+    }));
 }
 
 function renderPage(){
     movies.forEach(movie => {
+        //console.log(movie.genre_ids);
+        
+
+        /*
+        const movieGenres = [];
+        for (let i = 0; i < movie.genre_ids.length; i++) {
+            const genreId = movie.genre_ids[i];
+            
+            for (let j = 0; j < genres.length; j++) {
+                const genre = genres[j];
+                if(genre.id == genreId){
+                    movieGenres.push(genre);
+                }
+            }
+        }*/
+
+        /*
+        const movieGenres = genres.filter(function(genre){
+            return movie.genre_ids.indexOf(genre.id) !== -1;
+        });
+        */
+
+       const movieGenres = genres.filter(genre => movie.genre_ids.indexOf(genre.id) !== -1);
+       const movieGenreNames = movieGenres.map(genre => genre.name);
+       const movieGenresHtml = movieGenreNames.map(name => `<span class="badge badge-secondary">${name}</span>`);
+
         document.querySelector("#index .row").innerHTML += `
         <div class="col-md-4 mb-4">
             <div class="card">
@@ -27,9 +64,11 @@ function renderPage(){
                 <p class="card-text mb-4">${movie.overview}</p>
                 
                 <a href="?movie=${movie.id}" class="btn btn-primary">Details</a>
+
+                <div>${movieGenresHtml.join("")}</div>
             </div>
             </div>
         </div>
     `;
-    });
+    });    
 }
