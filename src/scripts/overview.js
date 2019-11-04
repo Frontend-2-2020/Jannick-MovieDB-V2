2817b53fdc5a565f4fc1e40e6cef3c0defd0cff9
jannick vandaele <jannickvandaele@hotmail.com>
2019-11-04 21:20:52 +0100

    pagination

--- src/scripts/overview.js
+++ src/scripts/overview.js
@@ -1,36 +1,107 @@
 import Axios from "axios";
 import { KEY } from "./config";
+import queryString from "query-string";
+
+const param = location.search;
+const parsed = queryString.parse(param);
+let page = parsed.page ? parseInt(parsed.page) : 1;
+
+let maxPages = 10 //default;
+let movies;
+let genres;
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const prevNumber = document.querySelector("#prevNumber a");
+const nextNumber = document.querySelector("#nextNumber a");
+const currentNumber = document.querySelector("#currentNumber span");
 
 export function initOverview() {
     document.getElementById("index").style.display = "block";
+
+    
+    prevBtn.addEventListener("click", prevPage);
+    nextBtn.addEventListener("click", nextPage);
+    prevNumber.addEventListener("click", prevPage);
+    nextNumber.addEventListener("click", nextPage);
+    
+    updatePagination();
+
     getData();
 }
 
+function prevPage(){
+    if(page > 1){
+        page -= 1;
+        updatePagination();
+        getMovies().then(response => {
+            movies = response.data.results;
+            renderPage();
+        })
+    }
+}
+
+function nextPage(){
+    if(page < 500){
+        page += 1;
+        updatePagination();
+        getMovies().then(response => {
+            movies = response.data.results;
+            renderPage();
+        })
+    }
+}
+
+function updatePagination(){
+    if(page === 1){
+        prevBtn.classList.add("disabled");
+        prevNumber.parentNode.style.display = "none";
+    } else {
+        prevNumber.parentNode.style.display = "block";
+        prevBtn.classList.remove("disabled");
+    }
+
+    if(page === maxPages){
+        nextBtn.classList.add("disabled");
+        nextNumber.parentNode.style.display = "none";
+    } else {
+        nextBtn.classList.remove("disabled");
+        nextNumber.parentNode.style.display = "block";
+    }
+
+    prevNumber.innerHTML = page - 1;
+    nextNumber.innerHTML = page + 1;
+    currentNumber.innerHTML = page;
+
+    const stringified = queryString.stringify({page: page});
+    if("?"+stringified !== location.search){
+        location.search = stringified;
+    }
+}
+
 function getMovies(){
-    return Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + KEY)
+    return Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=" + page + "&api_key=" + KEY)
 }
 
 function getGenres(){
     return Axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=" + KEY)
 }
 
-let movies;
-let genres;
 function getData(){
     Axios.all([getMovies(), getGenres()])
     .then(Axios.spread(function (moviesResponse, genresResponse) {
         movies = moviesResponse.data.results;
         genres = genresResponse.data.genres;
 
+        maxPages = moviesResponse.data.total_pages;
+
         renderPage();
     }));
 }
 
 function renderPage(){
-    movies.forEach(movie => {
-        //console.log(movie.genre_ids);
-        
+    document.querySelector("#index .row").innerHTML = "";
 
+    movies.forEach(movie => {
         /*
         const movieGenres = [];
         for (let i = 0; i < movie.genre_ids.length; i++) {
@@ -52,7 +123,7 @@ function renderPage(){
 
        const movieGenres = genres.filter(genre => movie.genre_ids.indexOf(genre.id) !== -1);
        const movieGenreNames = movieGenres.map(genre => genre.name);
-       const movieGenresHtml = movieGenreNames.map(name => `<span class="badge badge-secondary">${name}</span>`);
+       const movieGenresHtml = movieGenreNames.map(name => `<span class="badge badge-secondary mb-1 mr-1">${name}</span>`);
 
         document.querySelector("#index .row").innerHTML += `
         <div class="col-md-4 mb-4">
